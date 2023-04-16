e5a724848e5623abd060e525c4a632d82e1ed3af
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-04-16 10:18:42 +0530

    1:15:32

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,10 @@
 import userModel from "../models/userModel.js";
-import { hashpassword } from "../helper/authHelper.js";
+import { comparePassword, hashpassword } from "../helper/authHelper.js";
+import Jwt from "jsonwebtoken";
 
 export const registerController = async (req, res) => {
   try {
-    const {name, email, password, phone, address} = req.body;
+    const { name, email, password, phone, address } = req.body;
     if (!name) {
       return res.send({ error: "Name is Required !" });
     }
@@ -43,13 +44,62 @@ export const registerController = async (req, res) => {
       success: true,
       message: "user Register Successfully",
       user,
-      
     });
   } catch (error) {
-    console.log("error is : ",error);
+    console.log("error is : ", error);
     res.status(500).send({
       success: false,
       message: "Error in Registeration.",
+      error,
+    });
+  }
+};
+
+// POST LOGIN
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //validation
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    // check user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "email is not registered.",
+      });
+    }
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res
+        .status(200)
+        .send({ success: false, message: "Invalid Password" });
+    }
+    //token
+    const token = await Jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "Login successfully",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error login",
       error,
     });
   }
