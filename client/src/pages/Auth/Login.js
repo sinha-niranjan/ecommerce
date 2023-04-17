ee7d78c152624541176a2af4bdd65af3d82b3c83
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-04-17 09:42:53 +0530

    3:34

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate,useLocation } from "react-router-dom";
 import toast from "react-hot-toast";
 import "../../styles/AuthStyles.css";
 import { useAuth } from "../../context/auth";
@@ -14,6 +14,7 @@ const Login = () => {
 
 
   const navigate = useNavigate();
+  const location = useLocation();
 
   // Form  function
   const handleSubmit = async (e) => {
@@ -31,7 +32,7 @@ const Login = () => {
           token: res.data.token,
         });
         localStorage.setItem('auth',JSON.stringify(res.data))
-        navigate("/");
+        navigate(location.state || "/");
       } else {
         toast.error(res.data.message);
       }
