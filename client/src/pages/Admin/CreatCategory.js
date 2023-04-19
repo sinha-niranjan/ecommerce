847b554808661309457f81efe801509de4d281db
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-04-19 12:48:58 +0530

    6:25

--- client/src/pages/Admin/CreatCategory.js
+++ client/src/pages/Admin/CreatCategory.js
@@ -33,14 +33,14 @@ const CreatCategory = () => {
     }
   };
 
-  // get all cat
+  // get all category
   const getAllCategory = async () => {
     try {
       const { data } = await axios.get(
         `${process.env.REACT_APP_API}/api/v1/category/get-category`
       );
-      if (data.success) {
-        setCategories(data.category);
+      if (data?.success) {
+        setCategories(data?.category);
       }
     } catch (error) {
       console.log(error);
