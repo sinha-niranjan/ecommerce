2728fe6d7ce53cf379872450b713a5e97b007c09
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-04-26 22:16:17 +0530

    update

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ import productRoutes from "./routes/productRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 // configure env
 dotenv.config();
 
 // database config
 connectDB();
+
+// esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // rest object
 const app = express();
@@ -23,7 +28,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
@@ -32,7 +37,7 @@ app.use("/api/v1/product", productRoutes);
 
 // rest api
 app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // port
