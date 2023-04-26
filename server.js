3d0898d8fced34c411ebe1f27b7bb67d9f535019
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-04-26 19:47:10 +0530

    10:56

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ import morgan from "morgan";
 import { connect } from "mongoose";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import cors from "cors";
+import path from "path";
 
 // configure env
 dotenv.config();
@@ -22,15 +23,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 // routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/product",productRoutes);
+app.use("/api/v1/product", productRoutes);
 
 // rest api
-app.get("/", (req, res) => {
-  res.send(`<h1> Welcome to ecommerce app.</h1>`);
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
 });
 
 // port
